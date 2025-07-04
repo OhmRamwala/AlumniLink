077b75235ebe7d95e87981f5b3ffa5af0ef8e66d
Ohm <ohmramwala@gmail.com>
2025-07-04 09:33:56 +0000

    Add an option to turn it into dark theme beside the profile icon on nav

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -2,6 +2,7 @@
 
 import { useSidebar, SidebarTrigger } from '@/components/ui/sidebar';
 import { UserNav } from './user-nav';
+import { ThemeToggle } from '../theme-toggle';
 
 export function Header() {
   const { isMobile } = useSidebar();
@@ -10,6 +11,7 @@ export function Header() {
     <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:h-16 sm:px-6">
       {isMobile && <SidebarTrigger className="flex" />}
       <div className="flex-1" />
+      <ThemeToggle />
       <UserNav />
     </header>
   );
