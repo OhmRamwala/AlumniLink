dba3e313949364f265082b8bd75888167fa8d1d5
Ohm <ohmramwala@gmail.com>
2025-07-04 09:48:02 +0000

    the dark theme should not be applied on the main page if the user is not

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,9 +1,13 @@
 "use client"
 
 import * as React from "react"
+import { usePathname } from "next/navigation"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { type ThemeProviderProps } from "next-themes/dist/types"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  const pathname = usePathname()
+  const isPublicPage = ['/', '/login', '/signup', '/pending-verification'].includes(pathname)
+
+  return <NextThemesProvider {...props} forcedTheme={isPublicPage ? 'light' : undefined}>{children}</NextThemesProvider>
 }
